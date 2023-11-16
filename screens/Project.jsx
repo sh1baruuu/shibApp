389e78e6ebf75b51bac44b5a0ed4e95d2f4e9fe2
sh1baruuu <devillarudolphangelo@gmail.com>
2023-11-17 00:16:11 +0800

    updated

---
+++ screens/Project.jsx
@@ -0,0 +1,11 @@
+import { View } from 'react-native'
+
+function Project() {
+
+    return (<View className="flex-1 bg-purple-600">
+        
+    </View>
+  )
+}
+
+export default Project
--- screens/Tabs.jsx
+++ screens/Tabs.jsx
@@ -1,6 +1,8 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import Home from "./Home";
 import Profile from "./Profile";
+import Project from "./Project";
 
 const Tab = createBottomTabNavigator();
 
@@ -8,11 +10,74 @@ function Tabs() {
     return (
         <Tab.Navigator
             screenOptions={{
-                headerShown:false,
+                headerShown: false,
             }}
         >
-            <Tab.Screen name="Home" component={Home} />
-            <Tab.Screen name="Profile" component={Profile} />
+            <Tab.Screen
+                name="Home"
+                component={Home}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <Icon
+                            name="home"
+                            size={size}
+                            color={color}
+                        />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Forum"
+                component={Project}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <Icon
+                            name="forum"
+                            size={size}
+                            color={color}
+                        />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Add"
+                component={Project}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <Icon
+                            name="plus-circle-outline"
+                            size={size}
+                            color={color}
+                        />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Project"
+                component={Project}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <Icon
+                            name="package-variant"
+                            size={size}
+                            color={color}
+                        />
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Profile"
+                component={Profile}
+                options={{
+                    tabBarIcon: ({ color, size }) => (
+                        <Icon
+                            name="account"
+                            color={color}
+                            size={size}
+                        />
+                    ),
+                }}
+            />
         </Tab.Navigator>
     );
 }
